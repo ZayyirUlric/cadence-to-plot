7d63154ffcaf9004f3f36c49771dd96d34485ac9
ZayyirUlric <zayyircornelio@gmail.com>
2023-05-26 11:08:56 +0800

    changed output dir to out/ and added a combining script for adding mulptiple plots into one iamge. has legends.

--- combine.py
+++ combine.py
@@ -6,7 +6,7 @@ import matplotlib
 import numpy as np
 import glob
 
-x_intercept = 0
+x_intercept = None
 tolerance = 1e-11
 y_cheat = None
 
@@ -31,6 +31,9 @@ def format_si(value):
     return "{:.3f} {}".format(value, units[magnitude])
 
 input_csvs = glob.glob("csv/*.vcsv")
+
+pl, ax = pyplot.subplots()
+
 for csv_file in input_csvs:
     with open(csv_file, newline='') as file:
         for i, line in enumerate(file):
@@ -45,12 +48,13 @@ for csv_file in input_csvs:
         csv_name = (csv_file.split("csv\\")[1]).split(".csv")[0]
         data = p.read_csv(file, sep=',', skiprows=5)
 
+        
         for i in range(0,data.shape[1],2):
             plot_name_list = name_line.split(",")
             label_name_list = label_line.split(",")
             label_unit_list = label_unit_line.split(",")
 
-            pl, ax = pyplot.subplots()
+            plot_name = plot_name_list[i//2].replace(":","_").replace(";","").replace("/","!").replace('"',"'").replace('?',".")
 
             markers = []
             if (x_intercept is not None):
@@ -62,7 +66,7 @@ for csv_file in input_csvs:
                         markers.append(j)
                         break
             
-            pyplot.plot(data.iloc[:,0+i], data.iloc[:,1+i], color="#c3073f", markevery=markers, marker="o", markersize=12, markeredgecolor="#000000", linewidth=5)
+            pyplot.plot(data.iloc[:,0+i], data.iloc[:,1+i], markevery=markers, marker="o", markersize=12, markeredgecolor="#000000", linewidth=1, label=csv_name.split(".vcsv")[0])
             
             ax.yaxis.set_major_formatter(matplotlib.ticker.EngFormatter(unit=''))
             ax.xaxis.set_major_formatter(matplotlib.ticker.EngFormatter(unit=''))
@@ -71,7 +75,6 @@ for csv_file in input_csvs:
 
             pyplot.xlabel(f'{label_name_list[0+i].replace(";","")} ({label_unit_list[0+i].replace(";","")})', fontdict={'weight': 'extra bold'})
             pyplot.ylabel(f'{label_name_list[1+i]} ({label_unit_list[1+i]})', fontdict={'weight': 'extra bold'})
-            plot_name = plot_name_list[i//2].replace(":","_").replace(";","").replace("/","!").replace('"',"'").replace('?',".")
             pl.suptitle(plot_name, fontdict={'weight': 'extra bold'})
 
             if (label_name_list[0+i].replace(";","") == "freq"):
@@ -87,7 +90,8 @@ for csv_file in input_csvs:
                     pyplot.annotate(label, (list(data.iloc[:,0+i])[j], list(data.iloc[:,1+i])[j]), xytext=(-10,15), textcoords="offset points")
                 #ax.annotate(str(v), xy=(j,v), xytext=(0,0), textcoords='offset points')
 
-            pl.tight_layout()
-            pyplot.grid(True)
-            pl.savefig(f"out/{csv_name}-{plot_name}.png")
-            pl.clear()
+pl.tight_layout()
+pyplot.legend()
+pyplot.grid(True)
+pl.savefig(f"out/combined/{csv_name}-{plot_name}.png")
+pl.clear()
